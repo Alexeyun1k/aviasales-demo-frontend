81f8b28e5c6eddf7a83e04cb74182ca982047a86
Alexey <ardovalexey@gmail.com>
2018-02-14 04:41:45 +0300

    Delete unused import

--- src/Header/CityTo.js
+++ src/Header/CityTo.js
@@ -1,8 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 
-import arrows from "./img/arrows.svg";
-
 const Container = styled.article`
   display: flex;
   background: #ffffff;
