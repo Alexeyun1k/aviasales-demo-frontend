4e4bb416dd4d5d51b2459fa916f1e5ee8be1fac0
Alexey <ardovalexey@gmail.com>
2018-02-21 02:56:40 +0300

    refactor: add styled queries

--- src/queries.js
+++ src/queries.js
@@ -1,8 +1,20 @@
-const queries = {
+import { css } from "styled-components";
+
+const sizes = {
   sm: 576,
   md: 768,
   lg: 992,
   xl: 1200
 };
 
-export default queries;
+export default sizes;
+
+export const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (min-width: ${sizes[label]}px) {
+      ${css(...args)};
+    }
+  `;
+
+  return acc;
+}, {});
