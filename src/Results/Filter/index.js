fc298190917e6d9a6d3ed185c0153011e650055c
Alexey <ardovalexey@gmail.com>
2018-02-21 02:21:05 +0300

    feat: add filter placeholder

---
+++ src/Results/Filter/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Filter = styled.section`
+  margin-top: 56px;
+  background: #ffffff;
+  border-radius: 4px;
+  min-height: 300px;
+`;
+
+export default function Results() {
+  return <Filter />;
+}
