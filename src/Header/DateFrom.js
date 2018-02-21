b941e0dc9ff6c867e4f146ff2300b7cec35800f3
Alexey <ardovalexey@gmail.com>
2018-02-21 03:23:36 +0300

    feat: datepicker sketch

--- src/Header/DateFrom.js
+++ src/Header/DateFrom.js
@@ -1,10 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import Calendar from "react-day-picker";
+import "react-day-picker/lib/style.css";
 
 import calendar from "./img/calendar.svg";
 import queries from "../queries";
 
-const DatePicker = styled.button`
+const DatePicker = styled.div`
+  position: relative;
+`;
+
+const Button = styled.button`
   width: 100%;
   padding: 18px 0 18px 16px;
   text-align: left;
@@ -29,6 +35,56 @@ const DatePicker = styled.button`
   }
 `;
 
-export default props => {
-  return <DatePicker>Туда</DatePicker>;
-};
+const Dropdown = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: #ffffff;
+  box-shadow: 0px 0px 8px rgba(74, 74, 74, 0.2),
+    0px 2px 4px rgba(74, 74, 74, 0.2);
+  border-radius: 2px;
+`;
+
+// export default props => {
+//   return (
+//     <DatePicker>
+//       <Button>Туда</Button>
+//       <Dropdown>
+//         <Calendar />
+//       </Dropdown>
+//     </DatePicker>
+//   );
+// };
+
+export default class DateFrom extends React.Component {
+  state = {
+    isOpen: false
+  };
+
+  toggleOpen = () => {
+    this.setState({ isOpen: true });
+  };
+
+  toggleClose = () => {
+    this.setState({ isOpen: false });
+  };
+
+  render() {
+    return (
+      <DatePicker>
+        <Button
+          onClick={this.toggleOpen}
+          type="button"
+          onBlur={this.toggleClose}
+        >
+          Туда
+        </Button>
+        {this.state.isOpen && (
+          <Dropdown>
+            <Calendar />
+          </Dropdown>
+        )}
+      </DatePicker>
+    );
+  }
+}
