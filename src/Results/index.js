3c8c30c921b4170e54f878d75b3f16a5b289c966
Alexey <ardovalexey@gmail.com>
2018-02-21 05:28:31 +0300

    feat: mobile result page

--- src/Results/index.js
+++ src/Results/index.js
@@ -1,28 +1,75 @@
 import React from "react";
 import styled from "styled-components";
-import MobileCard from "./MobileCard/";
-import FullCard from "./FullCard/";
+import Card from "./Card/";
+import Header from "./Header/";
+import Filter from "./Filter/";
+import { results } from "../FakeData";
+import { media } from "../queries";
 
-const Section = styled.section`
+const Page = styled.div`
   min-height: 300px;
-  padding-top: 40px;
   padding-bottom: 40px;
   background: #eaeaea;
 `;
 
+const FilterButton = styled.button`
+  position: fixed;
+  bottom: 8px;
+  left: 50%;
+  transform: translateX(-50%);
+  border: none;
+  padding: 10px 24px;
+  background: #00ace2;
+  border-radius: 100px;
+  font-weight: 900;
+  font-size: 16px;
+  text-align: center;
+  color: #ffffff;
+`;
+
+const ToTop = styled.button`
+  display: block;
+  margin: 16px auto;
+  border: none;
+  padding: 6px 16px;
+  background: #00ace2;
+  opacity: 0.5;
+  border-radius: 100px;
+  font-weight: 900;
+  font-size: 14px;
+  text-align: center;
+  text-transform: uppercase;
+  color: #ffffff;
+
+  ${media.md`
+    display: none;
+  `};
+`;
+
 export default function Results() {
   return (
-    <Section>
-      <div className="hidden-md hidden-lg hidden-xl">
-        <MobileCard />
-        <MobileCard />
-        <MobileCard />
+    <Page>
+      <Header />
+
+      <ToTop>Наверх</ToTop>
+      <div class="container">
+        <div class="row">
+          <div class="hidden-xs hidden-sm hidden-md col-lg-4 col-xl-3">
+            <Filter />
+          </div>
+
+          <div class="col-sm-12 col-lg-8 col-xl-7">
+            {results.map(card => (
+              <Card
+                badge={card.badge}
+                terms={card.terms}
+                flight={card.flight}
+              />
+            ))}
+          </div>
+        </div>
+        <FilterButton>Фильтровать</FilterButton>
       </div>
-      <div className="hidden-xs hidden-sm col-md-12 col-lg-8 col-xl-7">
-        <FullCard />
-        <FullCard />
-        <FullCard />
-      </div>
-    </Section>
+    </Page>
   );
 }
