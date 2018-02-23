ce674402e8c00f27d78026996692f258748e79b8
Alexey <ardovalexey@gmail.com>
2018-02-24 02:49:48 +0300

    feat: sticky buttons

--- src/Results/index.js
+++ src/Results/index.js
@@ -6,13 +6,17 @@ import Filter from "./Filter/";
 import { results } from "../FakeData";
 import { media } from "../queries";
 
-const Page = styled.div`
+const Main = styled.main`
+  position: relative;
   min-height: 300px;
-  padding-bottom: 40px;
+  padding-bottom: 16px;
+  padding-top: 16px;
   background: #eaeaea;
 `;
 
 const Cards = styled.section`
+  display: flex;
+  flex-direction: column;
   margin-top: 8px;
 
   ${media.md`
@@ -25,10 +29,12 @@ const Cards = styled.section`
 `;
 
 const FilterButton = styled.button`
-  position: fixed;
+  display: block;
+  position: sticky;
   bottom: 8px;
-  left: 50%;
-  transform: translateX(-50%);
+  top: 8px;
+  margin: auto;
+  margin-top: 16px;
   border: none;
   padding: 10px 24px;
   background: #00ace2;
@@ -37,11 +43,23 @@ const FilterButton = styled.button`
   font-size: 16px;
   text-align: center;
   color: #ffffff;
+
+  ${media.md`
+    margin-top: 0px;
+    margin-bottom: 16px;
+  `};
+
+  ${media.lg`
+    display: none;
+  `};
 `;
 
 const ToTop = styled.button`
   display: block;
-  margin: 16px auto;
+  position: sticky;
+  top: 8px;
+  margin: auto;
+  margin-bottom: 16px;
   border: none;
   padding: 6px 16px;
   background: #00ace2;
@@ -58,33 +76,51 @@ const ToTop = styled.button`
   `};
 `;
 
+const More = styled.button`
+  display: block;
+  width: 100%;
+  margin-top: 16px;
+  border: none;
+  padding: 18px 32px;
+  background: #00ace2;
+  border-radius: 4px;
+  font-weight: 500;
+  line-height: 18px;
+  font-size: 14px;
+  text-align: center;
+  text-transform: uppercase;
+  color: #ffffff;
+`;
+
 export default function Results() {
   return (
-    <Page>
+    <div>
       <Header compact />
+      <Main>
+        <ToTop>Наверх</ToTop>
+        <div className="container">
+          <div className="row">
+            <div className="hidden-xs hidden-sm hidden-md col-lg-4 col-xl-3">
+              <Filter />
+            </div>
 
-      <ToTop>Наверх</ToTop>
-      <div className="container">
-        <div className="row">
-          <div className="hidden-xs hidden-sm hidden-md col-lg-4 col-xl-3">
-            <Filter />
-          </div>
-
-          <div className="col-sm-12 col-lg-8 col-xl-7">
-            <Cards>
-              {results.map((card, index) => (
-                <Card
-                  key={index}
-                  badge={card.badge}
-                  terms={card.terms}
-                  flight={card.flight}
-                />
-              ))}
-            </Cards>
+            <div className="col-sm-12 col-lg-8 col-xl-7">
+              <Cards>
+                {results.map((card, index) => (
+                  <Card
+                    key={index}
+                    badge={card.badge}
+                    terms={card.terms}
+                    flight={card.flight}
+                  />
+                ))}
+                <More>ПОКАЗАТЬ ЕЩЕ 10 БИЛЕТОВ!</More>
+                <FilterButton className="first-md">Фильтровать</FilterButton>
+              </Cards>
+            </div>
           </div>
         </div>
-        <FilterButton>Фильтровать</FilterButton>
-      </div>
-    </Page>
+      </Main>
+    </div>
   );
 }
