52b13d467a94e5b744304c58a1738d9517d3bf69
Alexey <ardovalexey@gmail.com>
2018-02-28 04:34:22 +0300

    refactor: separate component BaggageIcon

--- src/Results/Card/Terms/BaggageTabs/index.js
+++ src/Results/Card/Terms/BaggageTabs/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import handbag from "./img/handbag.svg";
-import baggage from "./img/baggage.svg";
-import no_baggage from "./img/no-baggage.svg";
+import BaggageIcon from "./BaggageIcon";
 
 const BaggageTabs = styled.div`
   padding-top: 12px;
@@ -10,48 +8,11 @@ const BaggageTabs = styled.div`
   justify-content: center;
 `;
 
-const Baggage = styled.div`
-  width: 24px;
-  height: 24px;
-  display: inline-block;
-  opacity: ${props => (props.weight === -1 ? 0.4 : 1)};
-  position: relative;
-  background-image: url(${props => (props.weight ? baggage : no_baggage)});
-  background-repeat: no-repeat;
-  background-position: center;
-
-  :after {
-    content: ${props =>
-      props.weight === -1
-        ? '"?"'
-        : props.weight ? '"' + props.weight + '"' : ""};
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 8px;
-    font-weight: bold;
-    line-height: normal;
-    text-align: center;
-    font-size: 10px;
-    text-align: center;
-    letter-spacing: -0.4px;
-    color: #9ab0b9;
-  }
-`;
-
-const Handbag = Baggage.extend`
-  background-image: url(${handbag});
-
-  :after {
-    top: 9px;
-  }
-`;
-
 export default ({ terms }) => {
   return (
     <BaggageTabs>
-      <Handbag weight={terms[0].handbag} />
-      <Baggage weight={terms[0].baggage} />
+      <BaggageIcon weight={terms[0].handbag} type="handbag" />
+      <BaggageIcon weight={terms[0].baggage} type="baggage" />
     </BaggageTabs>
   );
 };
